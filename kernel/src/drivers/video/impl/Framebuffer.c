2b84c2aa8cf1c004200a0ccd4a14da759788904f
Ian-Moffett <hscorp99906@gmail.com>
2022-03-12 07:58:14 -0500

    ..

--- kernel/src/drivers/video/impl/Framebuffer.c
+++ kernel/src/drivers/video/impl/Framebuffer.c
@@ -349,18 +349,3 @@ void init_framebuffer(framebuffer_t* lfb, void* addr, void* fb_core, void* kwrit
     lfb->fb_core = fb_core;
     lfb->kwrite = kwrite;
 }
-
-void putChar(framebuffer_t* framebuffer, psf1_font_t* psf1_font, unsigned int color, char chr, unsigned int xOff, unsigned int yOff) {
-    unsigned int* pixPtr = (unsigned int*)framebuffer->baseAddr;
-    char* fontPtr = psf1_font->glpyhBuffer + (chr * psf1_font->header->chsize);
-    for (unsigned long y = yOff; y < yOff + 16; y++){
-        for (unsigned long x = xOff; x < xOff+8; x++){
-            if ((*fontPtr & (0b10000000 >> (x - xOff))) > 0){
-                    *(unsigned int*)(pixPtr + x + (y * framebuffer->ppsl)) = color;
-                }
-
-        }
-
-        fontPtr++;
-    }
-}
