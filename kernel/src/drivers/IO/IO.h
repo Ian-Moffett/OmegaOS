49c7450abaeb3c7aa423850d4b3fd3db940ad744
Ian-Moffett <hscorp99906@gmail.com>
2022-03-12 14:10:44 -0500

    ..

--- kernel/src/drivers/IO/IO.h
+++ kernel/src/drivers/IO/IO.h
@@ -6,6 +6,8 @@
 
 void outportb(uint16_t port, uint8_t byte);
 uint8_t inportb(uint16_t port);
+uint32_t inportl(uint16_t port);
+uint32_t outportl(uint16_t port, uint32_t dword);
 void io_wait();
 
 
--- kernel/src/drivers/IO/impl/IO.c
+++ kernel/src/drivers/IO/impl/IO.c
@@ -15,6 +15,17 @@ uint8_t inportb(uint16_t port) {
 }
 
 
+uint32_t inportl(uint16_t port) {
+    uint32_t dword;
+    __asm__ __volatile__("in %%dx, %%al" : "=a" (dword) : "d" (port));
+    return dword;
+}
+
+uint32_t outportl(uint16_t port, uint32_t dword) {
+    __asm__ __volatile__("out %%al, %%dx" : : "a" (dword), "d" (port));
+}
+
+
 void io_wait() {
     // Waste one IO cycle.
     __asm__ __volatile__("outb %%al, $0x80" : : "a" (0));
